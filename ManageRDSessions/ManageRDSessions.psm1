c28d7a8cf74be048771abea7a3ef636dd58b1fdd
Steve Baker <steve_m_baker@hotmail.com>
2019-04-11 17:21:05 +0100

    Added pipeline and shouldprocess support

--- ManageRDSessions/ManageRDSessions.psm1
+++ ManageRDSessions/ManageRDSessions.psm1
@@ -1,71 +1,104 @@
-function Get-RDActiveSession {
-    $sessions = Get-RDUserSession | Where-Object { $_.SessionState -eq 'STATE_ACTIVE' -or $_.SessionState -eq 'STATE_IDLE' }
-    $sessions | Select-Object HostServer, UserName, UnifiedSessionId, SessionState | Format-Table
-}
-
-function Get-RDDisconnectedSession {
-    [CmdletBinding()]
-    param (
-        [
-        Parameter(
-            Mandatory = $False,
-            HelpMessage = "Specify minimum number of idle session minutes"
-        )
-        ]
-        [Alias('idle')]
-        [int]$MinIdleTime = 0
+function Get-sbRDSession {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Active", "Idle", "Connected", "Disconnected", "Any")]
+        [Alias("State")]
+        [string]$SessionState = "Any",
+        [Parameter(Mandatory = $false)]
+        [int]$MinimumIdleTime = 0
     )
 
-    $sessions = Get-RDUserSession | Where-Object { $_.SessionState -eq 'STATE_DISCONNECTED' -and ( $_.IdleTime / 60000 ) -gt $MinIdleTime }
-    $sessions | Select-Object HostServer, UserName, @{ Label = 'Idle Time (Mins)'; expression = {$_.IdleTime / 60000 -as [int] } }, UnifiedSessionId, SessionState | Format-Table
-}
-
-function Remove-RDDisconnectedSession {
-    [CmdletBinding()]
-    param (
-        [
-        Parameter(
-            Mandatory = $False,
-            HelpMessage = "Specify minimum number of idle session minutes"
-        )
-        ]
-        [Alias('idle')]
-        [int]$MinIdleTime = 0,
-        [switch]$Force = $False
-    )
-
-    $sessions = Get-RDUserSession | Where-Object { $_.SessionState -eq 'STATE_DISCONNECTED' -and ( $_.IdleTime / 60000 ) -gt $MinIdleTime }
-
-    foreach ( $session in $sessions ) {
-
-        $sessionParams = @{
-            HostServer       = $session.HostServer
-            UnifiedSessionID = $session.UnifiedSessionId
-            Force            = $Force
-        }
-        Invoke-RDUserLogoff @sessionParams
+    Begin {
+        # Intentionally empty
     }
-}
+    # Process block is required when outputting objects to the pipeline, otherwise only the final object will be passed
+    Process {
 
-function Disconnect-RDConnectedUserSession {
-    [CmdletBinding()]
+        $state = switch ($SessionState) {
+            "Active" { "STATE_ACTIVE" }
+            "Idle" { "STATE_IDLE" }
+            "Disconnected" { "STATE_DISCONNECTED" }
+            "Connected" { "STATE_CONNECTED" }
+            "Any" { "*" }
+        }
+        Write-Verbose "Querying RD Session Collection for $SessionState sessions"
+        $sessions = Get-RDUserSession | Where-Object {
+            $_.SessionState -like $state -and ( $_.IdleTime / 60000 ) -ge $MinimumIdleTime
+        }
+
+    foreach ($session in $sessions) {
+        # Creating PSObject to output and naming as my own type for use in later pipeline input...
+        $object = [PSCustomObject]@{
+            PSTypeName       = "Custom.SB.RDSession"
+            HostServer       = $session.HostServer
+            UserName         = $session.UserName
+            UnifiedSessionID = $session.UnifiedSessionID
+            SessionState     = $session.SessionState
+            IdleTime         = ($session.IdleTime / 60000 -as [int])
+        } #pscustomobject
+
+        # Outputting object
+        Write-Output $object
+    } #foreach
+} #process
+
+End {
+    #Intentionally empty
+}
+} #function
+
+function Remove-sbRDSession {
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
     param (
-        [switch]$Force = $False
+        # Accepting Pipeline ByValue and requiring custom Type - also added [Object[]] to make an array, not sure if needed
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [PSTypeName("Custom.SB.RDSession")][Object[]]$RDSession,
+        [Parameter(Mandatory = $true)]
+        [ValidateSet("LogOff", "Disconnect")]
+        [string]$Action
     )
 
-    $sessions = Get-RDUserSession | Where-Object { $_.SessionState -eq 'STATE_ACTIVE' -or $_.SessionState -eq 'STATE_IDLE' }
-
-    foreach ( $session in $sessions ) {
-        $sessionParams = @{
-            HostServer       = $session.HostServer
-            UnifiedSessionID = $session.UnifiedSessionId
-            Force            = $Force
-        }
-        Disconnect-RDUser @sessionParams
+    Begin {
+        # Intentionally empty
     }
-}
+    # Process block is required when receiving objects from the pipeline, otherwise only last object will be received
+    Process {
+        if ($PSCmdlet.ShouldProcess("RDUser: $($RDSession.Username) with Session ID $($RDSession.UnifiedSessionID)", "$Action")) {
 
-function Send-RDAllUserMessage {
+            $params = @{
+                'HostServer'       = $RDSession.HostServer
+                'UnifiedSessionID' = $RDSession.UnifiedSessionID
+                'ErrorAction'      = 'Stop'
+                'Force'            = $true
+            }
+
+            Write-Verbose "Attempting $Action of $($RDSession.Username) on $($RDSession.HostServer)"
+            switch ($Action) {
+                "LogOff" {
+                    Invoke-RDUserLogoff @params
+                    Write-Host "User [$($RDSession.Username)] [Logged Off] from [$($RDSession.HostServer)]"
+                }
+                "Disconnect" {
+                    Disconnect-RDUser @params
+                    Write-Host "User [$($RDSession.Username)] [Disconnected] from [$($RDSession.HostServer)]"
+                }
+                Default { }
+
+            }
+
+        } #shouldprocess
+
+    } #Process
+
+    End {
+        # Intentionally empty
+    }
+} #function
+
+
+<# Start of multi-line comment - BELOW TO BE DEVELOPED AND ACCEPT PIPELINE INPUT
+function Send-sbRDMessage {
     [CmdletBinding()]
     param (
         [
@@ -101,3 +134,5 @@ function Send-RDAllUserMessage {
         Send-RDUserMessage @messageParams
     }
 }
+
+End of multi-line comment #>
