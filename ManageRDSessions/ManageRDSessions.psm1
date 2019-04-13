35d8bf1306d113d4138c4b685e74b044d02408c2
Steve Baker <steve_m_baker@hotmail.com>
2019-04-13 11:54:22 +0100

    Changed switch to lookup table Added variable object array support

--- ManageRDSessions/ManageRDSessions.psm1
+++ ManageRDSessions/ManageRDSessions.psm1
@@ -16,17 +16,18 @@ function Get-sbRDSession {
     }
     # Process block is required when outputting objects to the pipeline, otherwise only the final object will be passed
     Process {
-
-        $state = switch ($SessionState) {
-            "Active" { "STATE_ACTIVE" }
-            "Idle" { "STATE_IDLE" }
-            "Disconnected" { "STATE_DISCONNECTED" }
-            "Connected" { "STATE_CONNECTED" }
-            "Any" { "*" }
+        # Variable Substitution alternative to Switch - suggested by Adam. Love this. It will convert whatever
+        # is passed after $stateLookup. into the value on the right.
+        $stateLookup = @{
+            Active       = "STATE_ACTIVE"
+            Idle         = "STATE_IDLE"
+            Disconnected = "STATE_DISCONNECTED"
+            Connected    = "STATE_CONNECTED"
+            Any          = "*"
         }
-        Write-Verbose "Querying RD Session Collection for $SessionState sessions"
+        Write-Verbose "Querying RD Session Collection for [$SessionState] sessions"
         $sessions = Get-RDUserSession | Where-Object {
-            $_.SessionState -like $state -and ( $_.IdleTime / 60000 ) -ge $MinimumIdleMins
+            $_.SessionState -like $stateLookup.$SessionState -and ( $_.IdleTime / 60000 ) -ge $MinimumIdleMins
         }
 
     foreach ($session in $sessions) {
@@ -54,7 +55,9 @@ function Remove-sbRDSession {
     # TO SPLIT INTO 2 DIVERSE FUNCTIONS - trying to do too much (see project)
     [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
     param (
-        # Accepting Pipeline ByValue and requiring custom Type - also added [Object[]] to make an array, not sure if needed
+        # Accepting Pipeline ByValue and requiring custom Type - also added [Object[]] to make an array, because this,
+        # when supported by a ForEach block in the receiving Function's Process block, will allow someone to output
+        # the objects to a variable, then pass that variable in to the receiving function.
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [PSTypeName("Custom.SB.RDSession")][Object[]]$RDSession,
 
@@ -68,31 +71,33 @@ function Remove-sbRDSession {
     }
     # Process block is required when receiving objects from the pipeline, otherwise only last object will be received
     Process {
-        if ($PSCmdlet.ShouldProcess("RDUser: $($RDSession.Username) with Session ID $($RDSession.UnifiedSessionID)", "$Action")) {
+        # ForEach needed in order to process an array of objects passed in as a variable, as opposed to the Pipeline which feeds objects individually
+        foreach ( $session in $RDSession ) {
+            if ($PSCmdlet.ShouldProcess("RDUser: [$($session.Username)] with Session ID [$($session.UnifiedSessionID)]", "[$Action]")) {
 
-            $params = @{
-                'HostServer'       = $RDSession.HostServer
-                'UnifiedSessionID' = $RDSession.UnifiedSessionID
-                'ErrorAction'      = 'Stop'
-                'Force'            = $true
-            }
-
-            Write-Verbose "Attempting $Action of $($RDSession.Username) on $($RDSession.HostServer)"
-            switch ($Action) {
-                "LogOff" {
-                    Invoke-RDUserLogoff @params
-                    Write-Host "User [$($RDSession.Username)] [Logged Off] from [$($RDSession.HostServer)]"
+                $params = @{
+                    'HostServer'       = $session.HostServer
+                    'UnifiedSessionID' = $session.UnifiedSessionID
+                    'ErrorAction'      = 'Stop'
+                    'Force'            = $true
                 }
-                "Disconnect" {
-                    Disconnect-RDUser @params
-                    Write-Host "User [$($RDSession.Username)] [Disconnected] from [$($RDSession.HostServer)]"
+
+                Write-Verbose "Attempting $Action of $($session.Username) on $($session.HostServer)"
+                switch ($Action) {
+                    "LogOff" {
+                        Invoke-RDUserLogoff @params
+                        Write-Host "User [$($session.Username)] [Logged Off] from [$($session.HostServer)]"
+                    }
+                    "Disconnect" {
+                        Disconnect-RDUser @params
+                        Write-Host "User [$($session.Username)] [Disconnected] from [$($session.HostServer)]"
+                    }
+                    Default { }
+
                 }
-                Default { }
 
-            }
-
-        } #shouldprocess
-
+            } #shouldprocess
+        } #foreach
     } #Process
 
     End {
