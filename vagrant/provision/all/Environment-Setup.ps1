2593c44ff9792756708c4fa822d2d23d4de5ba9b
Steve Baker <steve_m_baker@hotmail.com>
2019-04-27 14:46:44 +0100

    Changed $null to -not

--- vagrant/provision/all/Environment-Setup.ps1
+++ vagrant/provision/all/Environment-Setup.ps1
@@ -2,8 +2,7 @@
 $firewallRules = 'FPS-ICMP4-ERQ-In', 'FPS-ICMP4-ERQ-Out'
 Set-NetFirewallRule -Name $firewallRules -Enabled 'True'
 # Enable Remote xdscdiagnostics use
-if ((Get-NetFirewallRule -Name 'Service RemoteAdmin' -ErrorAction 'SilentlyContinue') -eq $null)
-{
+if (-not (Get-NetFirewallRule -Name 'Service RemoteAdmin' -ErrorAction 'SilentlyContinue')) {
     Write-Host "Creating new Firewall Rule 'Service RemoteAdmin'"
     New-NetFirewallRule -Name "Service RemoteAdmin" -DisplayName "Remote" -Action 'Allow'
 }
@@ -15,12 +14,10 @@ Write-Host 'Enabling DSC Analytic/Debug logs...' -ForegroundColor 'Yellow'
 # Log names
 $logNames = 'Operational', 'Analytic', 'Debug'
 # Disable logs first
-foreach ($logName in $logNames)
-{
+foreach ($logName in $logNames) {
     Update-xDscEventLogStatus -Channel $logName -Status 'Disabled' -ComputerName $computerName -Verbose
 }
 # Enable logs
-foreach ($logName in $logNames)
-{
+foreach ($logName in $logNames) {
     Update-xDscEventLogStatus -Channel $logName -Status 'Enabled' -ComputerName $computerName -Verbose
 }
