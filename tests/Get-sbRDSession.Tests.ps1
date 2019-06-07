800212fa01bd1ac1e5d02b4da02ce0e25d6b6513
Steve Baker <steve_m_baker@hotmail.com>
2019-06-07 14:25:50 +0100

    Fixed problems with Pester Test

--- tests/Get-sbRDSession.Tests.ps1
+++ tests/Get-sbRDSession.Tests.ps1
@@ -9,22 +9,8 @@
         ├───Get-Example.Tests.ps1
  #>
 
-# UNLESS you have a Vagrant setup with DSC "Examples" folders containing
-# other .psd1 files, in which case, put the modules in a subfolder and
-# adjust the depth of the $ModuleRoot declaration below (\*\*.psd1 for standard,
-# \*\*\*.psd1 for Vagrant/DSC labs)
-<#
-.\<Repo Root>
-    ├───Modules
-        ├───ModuleName
-            ├───Module.psm1
-            ├───Module.psd1
-    ├───tests
-        ├───Get-Example.Tests.ps1
- #>
-
 $ProjectRoot = Resolve-Path "$PSScriptRoot\.."
-$ModuleRoot = Split-Path (Resolve-Path "$ProjectRoot\*\*\*.psd1")
+$ModuleRoot = Split-Path (Resolve-Path "$ProjectRoot\*\*.psd1")
 $ModuleName = Split-Path $ModuleRoot -Leaf
 $ModulePath = (Join-Path $ModuleRoot "$ModuleName.psd1")
 Import-Module $ModulePath -Force
@@ -32,14 +18,14 @@ Import-Module $ModulePath -Force
 # When testing against an entire module, make sure to specify Module Scope
 InModuleScope $ModuleName {
     Describe "Get-sbRDSession" {
-        Mock Get-RdUserSession { return 1 }
+        Mock Get-RdUserSession { return 1 } -ModuleName ManageRDSessions
         It "gets all rdsessions from the RDS session collection" {
-            Get-sbRDSession -SessionState Active
+            Get-sbRDSession
             Assert-MockCalled Get-RdUserSession -Exactly 1 -Scope It
         }
 
         It "only accepts active, idle, disconnected, connected or any" {
             { Get-sbRDSession -SessionState Dancing } | Should Throw
-        }
     }
+}
 } #in modulescope
